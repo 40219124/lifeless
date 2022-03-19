d3d48d7aad38ebbb615a83fd0be6b560b4a544e0
Timothy Sanderson <t.s.sanderson@hotmail.co.uk>
2022-03-19 14:42:10 +0000

    Hide button colours on Start

--- LifelessUnity/Assets/Scripts/InteractionRelay.cs
+++ LifelessUnity/Assets/Scripts/InteractionRelay.cs
@@ -1,10 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InteractionRelay : MonoBehaviour
 {
     public string Setting = "";
+
+    private void Start()
+    {
+        foreach(InteractionFromButtonRelay br in GetComponentsInChildren<InteractionFromButtonRelay>())
+        {
+            Image image = br.GetComponent<Image>();
+            Color newColour = image.color;
+            newColour.a = 0;
+            image.color = newColour;
+        }
+    }
 
     public void PlaySceneDialogue()
     {
